da358db01d65643c59f57786f507c531e8e86a19
sporter399 <sjporter399@gmail.com>
2019-10-14 15:45:51 -0600

    3:45 commit of .kaggle

--- kaggleRead.py
+++ kaggleRead.py
@@ -1,47 +1,31 @@
 import pandas as pd
+import numpy as np
 import csv
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sqlite3
 
-
-
 trainDF = pd.read_csv('cs-test.csv')
 
-trainDF.head()
-
 app = Flask(__name__)
-
-
-
-
-
-@app.route('/rawdata', methods=['POST', 'GET'])
-def raw_data():
-
-    raw_array = []
-    raw_array.append(trainDF)
-    print("line 25 in python")
-    
-    return render_template('rawdata.html',raw_array=raw_array)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    output = trainDF.at[227, 'age']
+    age_var = []
 
-    raw_array = []
-    raw_array.append(trainDF)
-    print("raw_array in python code    " + str(raw_array))
-    print("line 33 in python")
+    for i in range(len(trainDF.index)):
+        ret_age = trainDF.at[i, 'age']
+        if ret_age < 50:
+            age_var.append(ret_age)
+        
+        
     
-    return render_template('base.html',len = len(raw_array),raw_array=raw_array)
-
-
-
-
-##print(trainDF)
+    
+    return render_template('base.html',output=output, age_var=age_var)
 
 if __name__ == '__main__':
     app.run()
