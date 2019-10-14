cc4d03997b8031aef774c53390b1f89b55502526
sporter399 <sjporter399@gmail.com>
2019-10-13 20:50:11 -0600

    8:50 commit of .kaggle

--- kaggleRead.py
+++ kaggleRead.py
@@ -1,10 +1,43 @@
 import pandas as pd
 import csv
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sqlite3
 
 
 
 trainDF = pd.read_csv('cs-test.csv')
+
 trainDF.head()
-trainDF.info()
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/rawdata', methods=['POST', 'GET'])
+def raw_data():
+
+    raw_array = []
+    raw_array.append(trainDF)
+    
+    
+    return render_template('rawdata.html',raw_array=raw_array)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    
+    
+    return render_template('rawdata.html')
+
+
+
 
 print(trainDF)
+
+if __name__ == '__main__':
+    app.run()
