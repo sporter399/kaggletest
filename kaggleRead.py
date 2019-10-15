3503a1ab76369cf8a16d87945ad38d6a5a7507fa
sporter399 <sjporter399@gmail.com>
2019-10-15 15:18:16 -0600

    3:18 commit of .kaggle

--- kaggleRead.py
+++ kaggleRead.py
@@ -4,14 +4,26 @@ import csv
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from pandas import DataFrame
 import os
 import sqlite3
 
 trainDF = pd.read_csv('cs-test.csv')
 
-engine = create_engine('sqlite://', echo=False)
-trainDF.to_sql('applicants', con=engine)
-engine.execute("SELECT * FROM applicants").fetchall()
+conn = sqlite3.connect('applicant_info.db')
+c = conn.cursor()
+c.execute('CREATE TABLE APPLICANTS (SeriousDlqin2yrs number,RevolvingUtilizationOfUnsecuredLines float, age number,NumberOfTime30to59DaysPastDueNotWorse number,DebtRatio float,MonthlyIncome number,NumberOfOpenCreditLinesAndLoans number,NumberOfTimes90DaysLate number,NumberRealEstateLoansOrLines number,NumberOfTime60to89DaysPastDueNotWorse number,NumberOfDependents number)')
+conn.commit()
+Applicants = trainDF
+
+trainDF = DataFrame(Applicants, columns= ['SeriousDlqin2yrs', 'RevolvingUtilizationOfUnsecuredLines', 'age', 'NumberOfTime30to59DaysPastDueNotWorse', 'DebtRatio', 'MonthlyIncome', 'NumberOfOpenCreditLinesAndLoans', 'NumberOfTimes90DaysLate', 'NumberRealEstateLoansOrLines', 'NumberOfTime60to89DaysPastDueNotWorse', 'NumberOfDependents'])
+trainDF.to_sql('APPLICANTS', conn, if_exists='replace', index = False)
+ 
+c.execute('''  
+SELECT * FROM APPLICANTS
+          ''')
+
+
 
 app = Flask(__name__)
 
